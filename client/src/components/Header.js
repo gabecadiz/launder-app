09e56b564634045cf163e9b0ea72a09932d3082c
Gabriel Cadiz <gabecadiz@hotmail.com>
2020-03-15 21:26:59 -0400

    update header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const Header = () => {
-  return <h2>Launder</h2>;
+  return;
+  <>
+    <h2>Launder </h2>
+    <p>{'exlusively for Gabe and Yena'}</p>
+  </>;
 };
